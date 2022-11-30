8742d4663c47883a4c68ba75d5b4f0389bf36d87
WindowJelly <ckdanr98@gmail.com>
2022-12-01 04:22:05 +0900

    창묵

--- demo 2/src/main/java/hufs/capstone/demo/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package hufs.capstone.demo.repository;
-
-import hufs.capstone.demo.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member,String> {
-//    @Query("SELECT * FROM USER WHERE nickname =?1")
-//    User findByNickname(String nickname);
-;}
