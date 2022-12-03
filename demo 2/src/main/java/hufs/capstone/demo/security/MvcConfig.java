a7060cbb40bd56a4f64ecd1524ba254bf51ba8cd
WindowJelly <ckdanr98@gmail.com>
2022-12-04 08:17:57 +0900

    파일 업로드 경로 수정

--- demo 2/src/main/java/hufs/capstone/demo/security/MvcConfig.java
+++ demo 2/src/main/java/hufs/capstone/demo/security/MvcConfig.java
@@ -1,23 +1,23 @@
-package hufs.capstone.demo.security;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class MvcConfig implements WebMvcConfigurer{
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new InterceptorConfig())
-                .order(0)
-                //적용할 url
-                .addPathPatterns("/**")
-                //제외할 url
-                .excludePathPatterns("/member/login" ,"/member/login/add","/member/find-all","/css/**", "/*.ico", "/error");
-    }
-    @Bean
-    public InterceptorConfig interceptorConfig(){
-        return new InterceptorConfig();
-    }
-}
+//package hufs.capstone.demo.security;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class MvcConfig implements WebMvcConfigurer{
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(new InterceptorConfig())
+//                .order(0)
+//                //적용할 url
+//                .addPathPatterns("/**")
+//                //제외할 url
+//                .excludePathPatterns("/member/login" ,"/member/login/add","/member/find-all","/css/**", "/*.ico", "/error");
+//    }
+//    @Bean
+//    public InterceptorConfig interceptorConfig(){
+//        return new InterceptorConfig();
+//    }
+//}
