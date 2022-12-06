5e69169d2f0d1e120fb8655b0cb000d3554a445f
jungh <junghahwang0601@gmail.com>
2022-12-07 05:50:44 +0900

    cors

--- demo 2/src/main/java/hufs/capstone/demo/security/MvcConfig.java
+++ demo 2/src/main/java/hufs/capstone/demo/security/MvcConfig.java
@@ -2,19 +2,29 @@ package hufs.capstone.demo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer{
+  @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*");
+    }
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new InterceptorConfig())
                 .order(0)
                 //적용할 url
-                .addPathPatterns("/**")
+                .addPathPatterns("/itempost/write", "/deliverypost/write","/itempost/update/**",
+                        "/deliverypost/update/**", "/itempost/delete", "/deliverypost/delete",
+                        "/member/mod/**", "/member/login/delete", "/member/mannerscore/**")
                 //제외할 url
-                .excludePathPatterns("/member/login" ,"/member/login/add","/member/find-all","/css/**", "/*.ico", "/error");
+                .excludePathPatterns("/member/login" ,"/member/login/add","/member/find-all","/css/**", "/*.ico", "/error",
+                        "/**", "/itempost/list/**", "/deliverypost/list/**");
     }
     @Bean
     public InterceptorConfig interceptorConfig(){
